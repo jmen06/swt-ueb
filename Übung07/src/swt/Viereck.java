10d25be7b3b976202170ba14dfd935b74efb9429
Johanna Engel <Johanna.Marie.Engel@mni.thm.de>
2013-11-26 11:30:57 +0100

    isParallelogramm() & isViereck() implementiert

---
+++ Übung07/src/swt/Viereck.java
@@ -0,0 +1,46 @@
+package swt;
+
+import java.awt.Point;
+
+public class Viereck {
+    private Point a;
+    private Point b;
+    private Point c;
+    private Point d;
+
+    public Viereck(Point a, Point b, Point c, Point d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    public boolean isQuadrat() {
+        if ((a.x == b.x) && (a.y == d.y) && (b.y == c.y) && (c.x == d.x)) {
+            int ab = Math.abs(a.y - b.y);
+            int bc = Math.abs(b.x - c.x);
+            int cd = Math.abs(c.y - d.y);
+            int da = Math.abs(d.x - a.x);
+            if (ab == bc && cd == da && bc == cd) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean isParallelogramm(){
+    	if((d.x-a.x)==(c.x-b.x) && (b.y-a.y)==(c.y-d.y)){
+    		return true;
+    	}else{
+		return false;}
+    	
+    }
+    
+    public boolean isViereck(){
+    	if(this.isQuadrat()==false && this.isRechteck()==false && this.isParallelogramm()==false
+    			&& this.isTrapez()==false){ 
+    				return true;
+    			}else{ return false;}
+    }
+   
+}
